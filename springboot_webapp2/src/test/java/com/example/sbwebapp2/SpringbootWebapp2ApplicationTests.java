90e7c90ad1498c893defd2a6c41a85b8b161638d
Tony Ghouse <tonyghouseofficial@gmail.com>
2021-03-05 16:04:46 +0530

    spring boot web app 2

---
+++ springboot_webapp2/src/test/java/com/example/sbwebapp2/SpringbootWebapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbwebapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
